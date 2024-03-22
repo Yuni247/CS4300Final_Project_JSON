83c7d5ecee02ad14dc41ebc79b77432b0c375e53
Joy <jl3335@cornell.edu>
2024-03-22 14:42:35 -0400

    added edit distance popup results

--- backend/app.py
+++ backend/app.py
@@ -4,10 +4,11 @@ from flask import Flask, render_template, request
 from flask_cors import CORS
 from helpers.MySQLDatabaseHandler import MySQLDatabaseHandler
 import pandas as pd
+import Levenshtein as lev
 
-# ROOT_PATH for linking with all your files. 
+# ROOT_PATH for linking with all your files.
 # Feel free to use a config.py or settings.py with a global export variable
-os.environ['ROOT_PATH'] = os.path.abspath(os.path.join("..",os.curdir))
+os.environ['ROOT_PATH'] = os.path.abspath(os.path.join("..", os.curdir))
 
 # Get the directory of the current script
 current_directory = os.path.dirname(os.path.abspath(__file__))
@@ -18,29 +19,40 @@ json_file_path = os.path.join(current_directory, 'init.json')
 # Assuming your JSON data is stored in a file named 'init.json'
 with open(json_file_path, 'r') as file:
     data = json.load(file)
-    episodes_df = pd.DataFrame(data['episodes'])
-    reviews_df = pd.DataFrame(data['reviews'])
+    books_df = pd.DataFrame(data)
 
 app = Flask(__name__)
 CORS(app)
 
 # Sample search using json with pandas
+
+
 def json_search(query):
-    matches = []
-    merged_df = pd.merge(episodes_df, reviews_df, left_on='id', right_on='id', how='inner')
-    matches = merged_df[merged_df['title'].str.lower().str.contains(query.lower())]
-    matches_filtered = matches[['title', 'descr', 'imdb_rating']]
-    matches_filtered_json = matches_filtered.to_json(orient='records')
-    return matches_filtered_json
+    books_df['Edit_distance'] = books_df['Title'].apply(
+        lambda book: lev.distance(query.lower(), book.lower()))
+
+    sorted_df = books_df.sort_values(by='Edit_distance', ascending=True)
+
+    ranked_results = sorted_df.head(10)
+    # for now round it to nearest 2 decimal places for uniformity, can change later
+    ranked_results['review_score'] = ranked_results['review_score'].round(2)
+
+    top_recs = ranked_results[[
+        'Title', 'descript', 'review_score']]
+
+    return top_recs.to_json(orient='records')
+
 
 @app.route("/")
 def home():
-    return render_template('base.html',title="sample html")
+    return render_template('base.html', title="sample html")
 
-@app.route("/episodes")
+
+@app.route("/books")
 def episodes_search():
     text = request.args.get("title")
     return json_search(text)
 
+
 if 'DB_NAME' not in os.environ:
-    app.run(debug=True,host="0.0.0.0",port=5000)
+    app.run(debug=True, host="0.0.0.0", port=5000)
