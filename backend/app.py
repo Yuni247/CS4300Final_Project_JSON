67843b4885cfe04d43893311db4e63d95930357e
Joy <jl3335@cornell.edu>
2024-04-29 00:40:26 -0400

    still need to fix svd json call

--- backend/app.py
+++ backend/app.py
@@ -18,11 +18,12 @@ current_directory = os.path.dirname(os.path.abspath(__file__))
 
 # Specify the path to the JSON file relative to the current script
 json_file_path = os.path.join(current_directory, 'init.json')
+books_df = pd.read_json(json_file_path)
 
 # Assuming your JSON data is stored in a file named 'init.json'
-with open(json_file_path, 'r') as file:
-    data = json.load(file)
-    books_df = pd.DataFrame(data)
+# with open(json_file_path, 'r') as file:
+#     data = json.load(file)
+#     books_df = pd.DataFrame(data)
 
 app = Flask(__name__, static_folder='static')
 CORS(app)
